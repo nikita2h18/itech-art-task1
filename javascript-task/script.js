15b8b709767a23752895a2bef06251d7bcfff132
Nikita Shaban <mikita.shaban@itechart-group.com>
2021-04-21 15:34:40 +0300

    js task: first algorithm

---
+++ javascript-task/script.js
@@ -0,0 +1,36 @@
+function task1() {
+    const arraysContainer = [
+        {v: [-1, 2, 5, 0, 3, -1, 4], expectedResult: 10},
+        {v: [1, -2, 3, 1, -9, 6], expectedResult: 6},
+        {v: [5, 11, -9, 20, 13], expectedResult: 33},
+    ];
+
+    function findBiggestSubSum() {
+        arraysContainer.forEach(
+            element => {
+                let biggestSum = 0;
+                let result = 0;
+                let sum = 0;
+                element.v.forEach(
+                    res => {
+                        sum += res;
+
+                        if (sum < result) {
+                            sum = 0;
+                        }
+
+                        result = sum;
+
+                        if (biggestSum < result) {
+                            biggestSum = result;
+                        }
+                    }
+                );
+
+                biggestSum === element.expectedResult ? console.log(biggestSum) : console.log("My algorithm wrong:(");
+            }
+        )
+    }
+
+    return findBiggestSubSum();
+}
