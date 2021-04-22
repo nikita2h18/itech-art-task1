01e89e648a6e3a3bc722edbddd00651fd07d60b8
Nikita Shaban <mikita.shaban@itechart-group.com>
2021-04-22 13:19:34 +0300

    js task: first algorithm, all subtasks done

--- javascript-task/script.js
+++ javascript-task/script.js
@@ -1,16 +1,19 @@
-const task1 = {
+const arraysProcessing = {
     arraysContainer: [
         {v: [-1, 2, 5, 0, 3, -1, 4], expectedResult: 13},
         {v: [1, -2, 3, 1, -9, 6], expectedResult: 6},
         {v: [5, 11, -9, 20, 13], expectedResult: 40},
     ],
 
-    findBiggestSubSum() {
-        this.arraysContainer.forEach(
+    findBiggestSubSum(container) {
+        container.forEach(
             element => {
+                console.log(element.v)
+
+                let copy = element.v;
                 let biggestSum = 0;
                 let partialSum = 0;
-                element.v.forEach(
+                copy.forEach(
                     res => {
                         partialSum += res;
 
@@ -27,14 +30,16 @@ const task1 = {
         )
     },
 
-    getMaxSubSum() {
-        this.arraysContainer.forEach(
+    getMaxSubSum(container) {
+        container.forEach(
             element => {
+                console.log(element.v)
+                let copy = element.v;
                 let biggestSum = 0;
-                for (let i = 0; i < element.v.length; i++) {
+                for (let i = 0; i < copy.length; i++) {
                     let sum = 0;
-                    for (let j = i; j < element.v.length; j++) {
-                        sum += element.v[j];
+                    for (let j = i; j < copy.length; j++) {
+                        sum += copy[j];
 
                         if (sum > biggestSum) {
                             biggestSum = sum;
@@ -46,4 +51,79 @@ const task1 = {
             }
         )
     },
+
+    search(container) {
+        container.forEach(
+            element => {
+                let copy = element.v;
+                let max = 0;
+                let min = copy[0];
+
+                copy.forEach(
+                    arrayElement => {
+                        if (max < arrayElement) {
+                            max = arrayElement
+                        }
+
+                        if (min > arrayElement) {
+                            min = arrayElement
+                        }
+                    }
+                )
+                console.log("max: " + max);
+                console.log("min: " + min);
+
+                copy.sort((a, b) => {
+                    return a - b;
+                });
+
+                let median = Math.floor(copy.length / 2);
+
+                if (copy.length % 2) {
+                    console.log("median: " + copy[median]);
+                } else {
+                    console.log("median: " + (copy[median - 1] + copy[median]) / 2.0);
+                }
+            }
+        )
+    },
+
+    selection(container) {
+        container.forEach(
+            element => {
+                console.log(element.v)
+
+                let selection = [];
+                let selectionResult = [];
+                for (let i = 0; i < element.v.length - 1; i++) {
+                    if (parseInt(element.v[i]) < parseInt(element.v[i + 1])) {
+                        selection.push(element.v[i]);
+                    } else {
+                        selection.push(element.v[i]);
+                        if (selectionResult.length < selection.length) {
+                            selectionResult = selection;
+                        }
+                        selection = [];
+                    }
+
+                    if (selectionResult.length < selection.length) {
+                        selectionResult = selection;
+                    }
+                }
+
+                console.log(selectionResult);
+            }
+        )
+    },
+
+    process() {
+        console.log("Sub sum O(n)");
+        this.findBiggestSubSum(this.arraysContainer);
+        console.log("Sub sum O(n^2)");
+        this.getMaxSubSum(this.arraysContainer);
+        console.log("Search");
+        this.search(this.arraysContainer);
+        console.log("Selection");
+        this.selection(this.arraysContainer);
+    }
 }
