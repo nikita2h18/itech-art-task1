64fc2b1e64d247e67c5ebcda264fa786784ea7fb
Nikita Shaban <mikita.shaban@itechart-group.com>
2021-04-22 11:34:57 +0300

    js task: first algorithm fixed

--- javascript-task/script.js
+++ javascript-task/script.js
@@ -1,34 +1,49 @@
 const task1 = {
     arraysContainer: [
-        {v: [-1, 2, 5, 0, 3, -1, 4], expectedResult: 10},
+        {v: [-1, 2, 5, 0, 3, -1, 4], expectedResult: 13},
         {v: [1, -2, 3, 1, -9, 6], expectedResult: 6},
-        {v: [5, 11, -9, 20, 13], expectedResult: 33},
+        {v: [5, 11, -9, 20, 13], expectedResult: 40},
     ],
 
     findBiggestSubSum() {
         this.arraysContainer.forEach(
             element => {
                 let biggestSum = 0;
-                let result = 0;
-                let sum = 0;
+                let partialSum = 0;
                 element.v.forEach(
                     res => {
-                        sum += res;
+                        partialSum += res;
 
-                        if (sum < result) {
-                            sum = 0;
+                        if (partialSum > biggestSum) {
+                            biggestSum = partialSum;
                         }
 
-                        result = sum;
-
-                        if (biggestSum < result) {
-                            biggestSum = result;
-                        }
+                        if (partialSum < 0) partialSum = 0;
                     }
                 );
 
                 biggestSum === element.expectedResult ? console.log(biggestSum) : console.log("My algorithm wrong:(");
             }
         )
-    }
+    },
 }
+
+
+function getMaxSubSum(arr) {
+    let sum = 0;
+    let biggestSum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        for (let p = i; p < arr.length; p++) {
+            sum += arr[p];
+            if (sum > biggestSum) {
+                biggestSum = sum;
+            }
+        }
+        sum = 0;
+    }
+    return (biggestSum > 0) ? biggestSum : -1;
+}
+
+task1.arraysContainer.forEach(
+    element => console.log(getMaxSubSum(element.v))
+)
