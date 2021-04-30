fe05bd8ac09b2c7e035f877cea9d54add11cb0e7
Nikita Shaban <mikita.shaban@itechart-group.com>
2021-04-30 16:53:22 +0300

    add breadth algorithm

--- links-getter/index.mjs
+++ links-getter/index.mjs
@@ -1,26 +1,35 @@
 import fetch from "node-fetch";
 
 let url = 'https://www.calhoun.io/';
-const getAllHrefs = /href\s*=\s*(['"])(https?:\/\/.+?)\1/ig;
-const getLinks = /(https?:\/\/[^\s]+)/g;
 
-const response = await fetch(url);
-const html = await response.text();
-const href = await html.match(getAllHrefs);
-let sitePages = [];
+async function breadthSearch(url) {
+    const getAllHrefs = /href\s*=\s*(['"])(https?:\/\/.+?)\1/ig;
+    const getLinks = /(https?:\/\/[^\s]+)\//g;
+    console.log('url ' + url);
 
-await href.forEach(e => {
-    if (e.includes(url)) {
-        sitePages.push(e);
+    const response = await fetch(url);
+    const html = await response.text();
+    const href = await html.match(getAllHrefs);
+    let sitePages = [];
+
+    await href.forEach(e => {
+        if (e.includes(url)) {
+            sitePages.push(e);
+        }
+    })
+
+    let links = []
+    sitePages.forEach(
+        link => links.push(link.match(getLinks))
+    )
+
+    console.log(links);
+
+    if (links.length === 0) {
+        return "Done";
+    } else {
+        links.forEach(link => breadthSearch(link));
     }
-})
+}
 
-let links = []
-sitePages.forEach(
-    link => {
-        links.push(link.match(getLinks))
-    }
-)
-
-console.log(links);
-
+breadthSearch(url).then(done => console.log(done));
