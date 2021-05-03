cb353d263d9b726f868ce23b62923d2fdc7eef45
Nikita Shaban <mikita.shaban@itechart-group.com>
2021-05-03 16:46:27 +0300

    fix algorithm bugs

--- links-getter/index.mjs
+++ links-getter/index.mjs
@@ -4,28 +4,20 @@ import pkg from "node-html-parser";
 const {parse} = pkg;
 
 const url = 'https://www.calhoun.io';
-const domain = 'calhoun.io'
-const visitedLinks = [];
+let domain = new URL(url);
+domain = domain.hostname.replace('www.', '');
+const links = new Map();
+let visitedLinks = [];
 
-async function getLinks(link) {
-    visitedLinks.push(link);
-
+async function seenLinks(link) {
+    visitedLinks.push(link)
     const htmlText = await getHtmlText(link);
     const htmlElements = parse(htmlText).querySelectorAll("a[href]");
-    let siteLinks = [];
-    htmlElements.forEach(
-        htmlElement => {
-            if (htmlElement._attrs.href.includes(domain) &&
-                !htmlElement._attrs.href.includes('mailto') ||
-                htmlElement._attrs.href[0] === '/') {
-                siteLinks.push(htmlElement._attrs.href)
-            }
-        }
-    );
+    const siteLinks = getSiteLinks(htmlElements);
 
     siteLinks.forEach(link => {
         if (!visitedLinks.includes(link) && link !== '/') {
-            getLinks(link)
+            seenLinks(link);
         }
     });
 }
@@ -42,4 +34,19 @@ async function getHtmlText(link) {
     return response.text();
 }
 
-getLinks(url).then(() => {console.log(visitedLinks)});
+function getSiteLinks(htmlElements) {
+    const siteLinks = [];
+    htmlElements.forEach(
+        htmlElement => {
+            if (htmlElement._attrs.href.includes(domain) &&
+                !htmlElement._attrs.href.includes('mailto') ||
+                htmlElement._attrs.href[0] === '/') {
+                siteLinks.push(htmlElement._attrs.href);
+            }
+        }
+    );
+
+    return siteLinks;
+}
+
+seenLinks(url).then(() => console.log(visitedLinks));
