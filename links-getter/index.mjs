80336b8527906e4c803e0ce5848c0ec7f3290cc3
Nikita Shaban <shaban.nikitka@gmail.com>
2021-05-05 18:17:58 +0300

    debug algorithm

--- links-getter/index.mjs
+++ links-getter/index.mjs
@@ -13,21 +13,19 @@ async function getVisitedLinks(link, visitedLinks = new Map()) {
     const response = await fetch(link);
     const htmlText = await getHtmlText(response);
     const htmlElements = parse(htmlText).querySelectorAll("a[href]");
-
-    getSiteLinks(htmlElements, visitedLinks);
-    if (allVisited(visitedLinks)) {
+    visitedLinks = getSiteLinks(htmlElements, visitedLinks);
+    if (isAllVisited(visitedLinks)) {
         return visitedLinks;
-    }
-
-    const links = [ ...visitedLinks.keys()];
-    links.forEach(l => {
-        if (!visitedLinks.get(l)) {
-            getVisitedLinks(l, visitedLinks);
+    } else {
+        for (let visitedLink of visitedLinks) {
+            if (!visitedLink[1]) {
+                getVisitedLinks(visitedLink[0], visitedLinks);
+            }
         }
-    })
+    }
 }
 
-function allVisited(visitedLinks) {
+function isAllVisited(visitedLinks) {
     for (let link of visitedLinks) {
         if (link[1] === false) {
             return false;
@@ -51,17 +49,23 @@ function isLinkValid(htmlElement) {
         && htmlElement._attrs.href !== '/';
 }
 
+function isNotVisited(link, visitedLinks) {
+    return !visitedLinks.has(link);
+}
+
 function getSiteLinks(htmlElements, visitedLinks) {
     htmlElements.forEach(
         htmlElement => {
+            let link = htmlElement._attrs.href;
             if (isLinkValid(htmlElement)) {
-                if (isRelative(htmlElement._attrs.href)) {
-                    htmlElement._attrs.href = url + htmlElement._attrs.href;
+                if (isRelative(link)) {
+                    link = url + link;
                 }
-                if (htmlElement._attrs.href[htmlElement._attrs.href.length - 1] === '/') {
-                    visitedLinks.set(htmlElement._attrs.href.substring(0, htmlElement._attrs.href.length - 1), false);
-                } else {
-                    visitedLinks.set(htmlElement._attrs.href, false);
+                if (link[link.length - 1] === '/') {
+                    link = link.substring(0, link.length - 1);
+                }
+                if (isNotVisited(link, visitedLinks)) {
+                    visitedLinks.set(link, false);
                 }
             }
         }
