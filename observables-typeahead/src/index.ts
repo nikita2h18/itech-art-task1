ff2024906f9f6d750e33c7604ed095f67d73d44a
Nikita Shaban <shaban.nikitka@gmail.com>
2021-05-26 14:35:11 +0300

    add algorithm

--- observables-typeahead/src/index.ts
+++ observables-typeahead/src/index.ts
@@ -1,7 +1,46 @@
 import {ajax} from 'rxjs/ajax';
-import {tap} from 'rxjs/operators';
+import {fromEvent} from 'rxjs';
+import {debounceTime, distinctUntilChanged, map, switchMap, tap} from 'rxjs/operators';
 
-ajax(`https://jsonplaceholder.typicode.com/users`).pipe(
-    tap(userResponse => console.log('users: ', userResponse)),
-);
+const getUsers = () => {
+    return ajax(`https://jsonplaceholder.typicode.com/users`)
+}
 
+const getContinents = (keys: string) => {
+
+    return [
+        'africa',
+        'antarctica',
+        'asia',
+        'australia',
+        'europe',
+        'north america',
+        'south america'
+    ].filter(e => e.indexOf(keys.toLowerCase()) > -1);
+};
+
+const searchUsers = (users: any, keys: string) => {
+    console.log(users);
+    return users.filter((e: string | string[]) => e.indexOf(keys.toLowerCase()) > -1);
+}
+
+const logUsers = (keys: string) => getUsers()
+    .pipe(
+        tap((ajaxResponse: any) => {
+            return ajaxResponse.response
+                .map(({username}: any) => console.log(username));
+        })
+    );
+
+fromEvent(document.getElementById('type-ahead'), 'keyup')
+    .pipe(
+        debounceTime(200),
+        map((e: any) => e.target.value),
+        distinctUntilChanged(),
+        switchMap(logUsers),
+        tap(c => {
+            console.log(c);
+            const usernames = c.response.map(({username}: any) => username);
+            return document.getElementById('output').innerText = usernames.join('\n');
+        })
+    ).subscribe();
