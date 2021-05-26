6796c939e9f0330684484ae62bcdefa506d93fd5
Nikita Shaban <shaban.nikitka@gmail.com>
2021-05-26 17:10:05 +0300

    refactoring

--- observables-typeahead/src/index.ts
+++ observables-typeahead/src/index.ts
@@ -2,20 +2,25 @@ import {ajax, AjaxResponse} from 'rxjs/ajax';
 import {fromEvent} from 'rxjs';
 import {debounceTime, distinctUntilChanged, map, switchMap, tap} from 'rxjs/operators';
 
-const getUsernames = (keys: string) => {
-    return ajax(`https://jsonplaceholder.typicode.com/users`).pipe(
-        map((ajaxResponse: AjaxResponse<any>) => ajaxResponse.response
-            .map((user: { username: string }) => user.username)
-            .filter((username: string) => username.toLowerCase().indexOf(keys.toLowerCase()) > -1)
-        )
-    )
+const getUsernamesByKeys = (keys: string) => ajax(`https://jsonplaceholder.typicode.com/users`)
+    .pipe(
+        map((ajaxResponse: AjaxResponse<any>) => ajaxResponse.response),
+        map(users => users.map((user: { username: string }) => user.username)),
+        map((usernames: string[]) => filterUsersByKeys(usernames, keys))
+    );
+
+const filterUsersByKeys = (usernames: string[], keys: string) => {
+    return usernames.filter((username: string) => hasKeys(username, keys));
+}
+
+const hasKeys = (username: string, keys: string) => {
+    return username.toLowerCase().indexOf(keys.toLowerCase()) > -1;
 }
 
 fromEvent(document.getElementById('type-ahead'), 'keyup')
     .pipe(
         debounceTime(200),
-        map((e: any) => e.target.value),
+        map((e) => (e.target as HTMLInputElement).value),
         distinctUntilChanged(),
-        switchMap(getUsernames),
-        tap((c: any) => document.getElementById('output').innerText = c.join('\n'))
-    ).subscribe();
+        switchMap(getUsernamesByKeys),
+    ).subscribe((filteredUsernames) => document.getElementById('output').innerText = filteredUsernames.join('\n'));
