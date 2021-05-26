c4becb6f7bb6cd94a19e5ff7ec6de8eced2e781b
Nikita Shaban <shaban.nikitka@gmail.com>
2021-05-26 15:27:06 +0300

    Search typeahead

--- observables-typeahead/src/index.ts
+++ observables-typeahead/src/index.ts
@@ -1,46 +1,23 @@
-import {ajax} from 'rxjs/ajax';
+import {ajax, AjaxResponse} from 'rxjs/ajax';
 import {fromEvent} from 'rxjs';
 import {debounceTime, distinctUntilChanged, map, switchMap, tap} from 'rxjs/operators';
 
-const getUsers = () => {
-    return ajax(`https://jsonplaceholder.typicode.com/users`)
+const getUsernames = (keys: string) => {
+    return ajax(`https://jsonplaceholder.typicode.com/users`).pipe(
+        map((ajaxResponse: AjaxResponse<any>) => ajaxResponse.response
+            .map((user: { username: string }) => user.username)
+            .filter((username: string) => {
+                return username.toLowerCase().indexOf(keys.toLowerCase()) > -1;
+            })
+        )
+    )
 }
-
-const getContinents = (keys: string) => {
-
-    return [
-        'africa',
-        'antarctica',
-        'asia',
-        'australia',
-        'europe',
-        'north america',
-        'south america'
-    ].filter(e => e.indexOf(keys.toLowerCase()) > -1);
-};
-
-const searchUsers = (users: any, keys: string) => {
-    console.log(users);
-    return users.filter((e: string | string[]) => e.indexOf(keys.toLowerCase()) > -1);
-}
-
-const logUsers = (keys: string) => getUsers()
-    .pipe(
-        tap((ajaxResponse: any) => {
-            return ajaxResponse.response
-                .map(({username}: any) => console.log(username));
-        })
-    );
 
 fromEvent(document.getElementById('type-ahead'), 'keyup')
     .pipe(
         debounceTime(200),
         map((e: any) => e.target.value),
         distinctUntilChanged(),
-        switchMap(logUsers),
-        tap(c => {
-            console.log(c);
-            const usernames = c.response.map(({username}: any) => username);
-            return document.getElementById('output').innerText = usernames.join('\n');
-        })
+        switchMap(getUsernames),
+        tap((c: any) => document.getElementById('output').innerText = c.join('\n'))
     ).subscribe();
